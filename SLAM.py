34f625b7f1da052ed44ff1c8e4e0b17be6946381
Erwin Terpstra <erwinterpstra@gmail.com>
2024-11-09 16:47:58 +0100

    benchmarking and some optimization

--- SLAM.py
+++ SLAM.py
@@ -2,9 +2,11 @@ import load_data as ld
 import numpy as np
 from SLAM_helper import *
 import matplotlib.pyplot as plt
+import matplotlib.animation as animation
 import MapUtils as maput
 import cv2
 import random
+import time
 
 #### Dataset ####
 
@@ -14,8 +16,9 @@ lid = ld.get_lidar("data/train_lidar2")
 #### Settings ###
 
 N, N_threshold = 100, 35
-step = 1
-sample_limit = None
+samples_per_step = 10
+samples_per_frame = 10
+frame_limit = None
 
 ### Data setup ##
 
@@ -75,112 +78,167 @@ mapfig = drawMap(particles[0, :], posX_map[0], posY_map[0], mapfig)
 
 pose_p, yaw_p = lid_p['pose'], rpy_p[0, 2]
 
+# Time keeping
+start_time = time.time()
+correlation_time = 0
+resample_time = 0
+map_draw_time = 0
+map_convert_time = 0
+update_particles_time = 0
+update_weights_time = 0
+
 # Loop over all samples in dataset
-timeline = min(sample_limit, len(lid)) if sample_limit else len(lid)
-for sample in range(1, timeline, step):
-	print("{0}/{1}".format(sample, timeline))
+timeline = min(frame_limit, len(lid)) if frame_limit else len(lid)
+sample = 1
 
-	# Get current lidar data and pose measurement
-	lid_c = lid[sample]
-	pose_c, rpy_c = lid_c['pose'], lid_c['rpy']
-	yaw_c = rpy_c[0, 2]
+#for sample in range(1, timeline, samples_per_step):
+def animate(frame):
+	global particles, weight
+	global ts, h_angle, rpy_robot
+	global lid_p, rpy_p, ind_0
+	global pos_phy, posX_map, posY_map
+	global mapfig
+	global pose_p, yaw_p
+	global sample
+	global correlation_time, resample_time, map_draw_time, map_convert_time, update_particles_time, update_weights_time
 
-	yaw_est = particles[:, 2]
+	next_frame = sample + samples_per_frame * samples_per_step
+	while sample < next_frame and sample < timeline:
+		# Get current lidar data and pose measurement
+		lid_c = lid[sample]
+		pose_c, rpy_c = lid_c['pose'], lid_c['rpy']
+		scan_c = lid_c['scan']
+		yaw_c = rpy_c[0, 2]
 
-	# This does some sort of reference frame conversion between previous pose and the particles
-	delta_x_gb = pose_c[0][0] - pose_p[0][0]
-	delta_y_gb = pose_c[0][1] - pose_p[0][1]
-	delta_theta_gb = yaw_c - yaw_p
+		yaw_est = particles[:, 2]
 
-	delta_x_lc = (np.cos(yaw_p) * delta_x_gb) + (np.sin(yaw_p) * delta_y_gb)
-	delta_y_lc = (-np.sin(yaw_p) * delta_x_gb) + (np.cos(yaw_p) * delta_y_gb)
-	delta_theta_lc = delta_theta_gb
+		# This does some sort of reference frame conversion between previous pose and the particles
+		update_particles_start = time.time()
+		delta_x_gb = pose_c[0][0] - pose_p[0][0]
+		delta_y_gb = pose_c[0][1] - pose_p[0][1]
+		delta_theta_gb = yaw_c - yaw_p
 
-	delta_x_gb_new = ((np.cos(yaw_est) * delta_x_lc) - (np.sin(yaw_est) * delta_y_lc)).reshape(-1, N)
-	delta_y_gb_new = ((np.sin(yaw_est) * delta_x_lc) + (np.cos(yaw_est) * delta_y_lc)).reshape(-1, N)
-	delta_theta_gb_new = np.tile(delta_theta_lc, (1, N))
+		delta_x_lc = (np.cos(yaw_p) * delta_x_gb) + (np.sin(yaw_p) * delta_y_gb)
+		delta_y_lc = (-np.sin(yaw_p) * delta_x_gb) + (np.cos(yaw_p) * delta_y_gb)
+		delta_theta_lc = delta_theta_gb
 
-	# I suspect this gives us the position and heading delta for each particle based on robot measurements
-	ut = np.concatenate([np.concatenate([delta_x_gb_new, delta_y_gb_new], axis=0), delta_theta_gb_new], axis=0)
-	ut = ut.T
+		delta_x_gb_new = ((np.cos(yaw_est) * delta_x_lc) - (np.sin(yaw_est) * delta_y_lc)).reshape(-1, N)
+		delta_y_gb_new = ((np.sin(yaw_est) * delta_x_lc) + (np.cos(yaw_est) * delta_y_lc)).reshape(-1, N)
+		delta_theta_gb_new = np.tile(delta_theta_lc, (1, N))
 
-	# Add the robot movement to each particle position and heading
-	# Also add a little bit of noise 
-	noise = np.random.normal(0, 1e-3, (N, 1)) * factor
-	particles = particles + ut + noise
+		# I suspect this gives us the position and heading delta for each particle based on robot measurements
+		ut = np.concatenate([np.concatenate([delta_x_gb_new, delta_y_gb_new], axis=0), delta_theta_gb_new], axis=0)
+		ut = ut.T
 
-	scan_c = lid_c['scan']
-	ind_i = np.argmin(np.absolute(ts - lid_c['t'][0][0]))
-	pos_phy, posX_map, posY_map = mapConvert(scan_c, rpy_robot[:, ind_i], h_angle[:, ind_i], angles, particles, N, pos_phy, posX_map, posY_map, mapfig)
+		# Add the robot movement to each particle position and heading
+		# Also add a little bit of noise 
+		noise = np.random.normal(0, 1e-3, (N, 1)) * factor
+		particles = particles + ut + noise
+		update_particles_time += time.time() - update_particles_start
 
-	# For each particle, calculate the correlation with the current map
-	corr = np.zeros((N, 1))
-	for i in range(N):
-		size = pos_phy[i].shape[1]
-		Y = np.concatenate([pos_phy[i], np.zeros((1, size))], axis = 0)
-		corr_cur = maput.mapCorrelation(mapfig['map'], x_im, y_im, Y[0 : 3, :], x_range, y_range)
-		ind = np.argmax(corr_cur)
+		map_convert_start = time.time()
+		ind_i = np.argmin(np.absolute(ts - lid_c['t'][0][0]))
+		pos_phy, posX_map, posY_map = mapConvert(scan_c, rpy_robot[:, ind_i], h_angle[:, ind_i], angles, particles, N, pos_phy, posX_map, posY_map, mapfig)
+		map_convert_time += time.time() - map_convert_start
 
-		corr[i] = corr_cur[ind / 3, ind % 3]
-		particles[i, 0] += x_range[ind / 3]
-		particles[i, 1] += y_range[ind % 3]
+		# For each particle, calculate the correlation with the current map
+		corr_start = time.time()
+		corr = np.zeros((N, 1))
+		for i in range(N):
+			size = pos_phy[i].shape[1]
+			Y = np.concatenate([pos_phy[i], np.zeros((1, size))], axis = 0)
+			corr_cur = maput.mapCorrelation(mapfig['map'], x_im, y_im, Y[0 : 3, :], x_range, y_range)
+			ind = np.argmax(corr_cur)
 
-	# Update weight of each particle according to correlation
-	weight = updateWeights(weight, corr)
+			corr[i] = corr_cur[ind / 3, ind % 3]
+			particles[i, 0] += x_range[ind / 3]
+			particles[i, 1] += y_range[ind % 3]
 
-	# Determine index of best particle
-	ind_best = weight.argmax()
+		# Keep track of how much time we spend on particle correlation
+		correlation_time += time.time() - corr_start
 
-	# Convert location of best particle to map coordinates
-	x_r = (np.ceil((particles[ind_best, 0] - mapfig['xmin']) / mapfig['res']).astype(np.int16) - 1)
-	y_r = (np.ceil((particles[ind_best, 1] - mapfig['xmin']) / mapfig['res']).astype(np.int16) - 1)
+		# Update weight of each particle according to correlation
+		update_weights_start = time.time()
+		weight = updateWeights(weight, corr)
+		update_weights_time += time.time() - update_weights_start
 
-	# Mark location with a blue pixel (index 0 in BGR)
-	mapfig['show_map'][x_r, y_r, 0] = 255
+		# Determine index of best particle
+		map_draw_start = time.time()
+		ind_best = weight.argmax()
 
-	# Draw map 
-	# TODO: Why is best particle passed here? What is the difference between particles array and posX_map/posY_Map
-	mapfig = drawMap(particles[ind_best, :], posX_map[ind_best], posY_map[ind_best], mapfig)
+		# Convert location of best particle to map coordinates
+		x_r = (np.ceil((particles[ind_best, 0] - mapfig['xmin']) / mapfig['res']).astype(np.int16) - 1)
+		y_r = (np.ceil((particles[ind_best, 1] - mapfig['xmin']) / mapfig['res']).astype(np.int16) - 1)
 
-	# Keep track of previous pose and yaw
-	pose_p, yaw_p = pose_c, yaw_c
+		# Mark location with a blue pixel (index 0 in BGR)
+		mapfig['show_map'][x_r, y_r, 0] = 255
 
-	# Resample particles if sum of squared weights is lower than threshold
-	# Since weights always sum to 1, this seems some sort of measure of whether the 
-	# weight distribution is broad or narrow
-	N_eff = 1 / np.sum(np.square(weight))
-	if N_eff < N_threshold:
-		print("Resampling ({0:.2f}/{1})".format(N_eff, N_threshold))
+		# Draw map 
+		# TODO: Why is best particle passed here? What is the difference between particles array and posX_map/posY_Map
+		mapfig = drawMap(particles[ind_best, :], posX_map[ind_best], posY_map[ind_best], mapfig)
+		map_draw_time = time.time() - map_draw_start
 
-		# Prepare a new list of particles
-		particle_New = np.zeros((N, 3))
+		# Keep track of previous pose and yaw
+		pose_p, yaw_p = pose_c, yaw_c
 
-		# TODO: Figure out how this resamples particles?
-		# It seems to mostly re-order particles?
-		r = random.uniform(0, 1.0 / N)
+		# Resample particles if sum of squared weights is lower than threshold
+		# Since weights always sum to 1, this seems some sort of measure of whether the 
+		# weight distribution is broad or narrow. A higher value would mean more of the weight 
+		# is "concentrated" in fewer particles, thus we have more effective particles
+		resample_start = time.time()
+		N_eff = 1 / np.sum(np.square(weight))
+		if N_eff < N_threshold:
+			#print("Resampling ({0:.2f}/{1})".format(N_eff, N_threshold))
 
-		c, i = weight[0], 0
-		for m in range(N):
-			u = r + m * (1.0 / N)
+			# Prepare a new list of particles
+			particle_New = np.zeros((N, 3))
+
+			# TODO: Figure out how this resamples particles?
+			# It seems to mostly re-order particles?
+			r = random.uniform(0, 1.0 / N)
+
+			c, i = weight[0], 0
+			for m in range(N):
+				u = r + m * (1.0 / N)
+				
+				while u > c:
+					i = i + 1
+					c = c + weight[i]
+
+				# NOTE: this line was unindented, so it only ran at the end of the loop
+				# That seems like a bug to me, otherwise particles_New would mostly be empty
+				particle_New[m, :] = particles[i, :]
+
+			particles = particle_New
 			
-			while u > c:
-				i = i + 1
-				c = c + weight[i]
+			# Reset the weight array to be uniform again
+			# (Weights for new particles are calculated next iteration)
+			weight = np.ones((N, 1)) * (1.0 / N)
 
-			# NOTE: this line was unindented, so it only ran at the end of the loop
-			# That seems like a bug to me, otherwise particles_New would mostly be empty
-			particle_New[m, :] = particles[i, :]
+		resample_time += time.time() - resample_start
 
-		particles = particle_New
-		
-		# Reset the weight array to be uniform again
-		# (Weights for new particles are calculated next iteration)
-		weight = np.ones((N, 1)) * (1.0 / N)
+		sample += samples_per_step
 
-	#im.set_data(mapfig['show_map'])
+		# This makes rendering a bit slower, but keeps the GUI responsive
+		plt.pause(0.001)
+
+	im.set_data(mapfig['show_map'])
 
 	#fig.canvas.draw()
 	#fig.canvas.flush_events()
+
+	# Print timing stats
+	elapsed_time = time.time() - start_time
+	elapsed_samples = sample / float(samples_per_step)
+
+	print("{0}/{1}".format(sample, timeline))
+	print("Total time: {:.1f}s; Avg. per frame: {:.1f}ms".format(elapsed_time, (elapsed_time * 1000) / elapsed_samples))
+	print("Correlation: {:.1f}ms; Resample: {:.1f}ms".format(correlation_time * 1000 / elapsed_samples, resample_time * 1000 / elapsed_samples))
+	print("Map draw: {:.1f}ms; Map convert: {:.1f}ms;".format(map_draw_time * 1000 / elapsed_samples, map_convert_time * 1000 / elapsed_samples))
+	print("Update particles: {:.1f}ms; Update weights: {:.1f}ms".format(update_particles_time * 1000 / elapsed_samples, update_weights_time * 1000 / elapsed_samples))
+	print("")
+
+	return im
 
 
 fig = plt.figure(1)
@@ -188,7 +246,9 @@ ax = fig.add_subplot(1, 1, 1)
 ax.set_title("SLAM Map")
 
 im = ax.imshow(mapfig['show_map'], cmap = "hot")
-plt.show()
 
-##plt.imshow(mapfig['show_map'], cmap = "hot")
-##plt.show()
+# Setup the animation
+frame_count = int(np.ceil((timeline - 1) / samples_per_step / samples_per_frame))
+anim = animation.FuncAnimation(fig=fig, func=animate, frames=frame_count, interval=0)
+
+plt.show()
--- SLAM_helper.py
+++ SLAM_helper.py
@@ -19,38 +19,61 @@ def updateWeights(weights, corr):
 	# Convert the weights back from log space to "normal" space
 	return np.exp(wtmp)
 
+def getW2B(part_cur, ori_robot):
+	r, p, y = ori_robot[0], ori_robot[1], part_cur[2]
+
+	cosY = np.cos(y)
+	sinY = np.sin(y)
+
+	cosP = np.cos(p)
+	sinP = np.sin(p)
+
+	cosR = np.cos(r)
+	sinR = np.sin(r)
+
+	r11 = cosY * cosP
+	r12 = cosY * sinP * sinR - sinY * cosR
+	r13 = cosY * sinP * cosR + sinY * sinR
+
+	r21 = sinY * cosP
+	r22 = sinY * sinP * sinR + cosY * cosR
+	r23 = sinY * sinP * cosR - cosY * sinR
+
+	r31 = -sinP
+	r32 = cosP * sinR
+	r33 = cosP * cosR
+	
+	t_w2b = np.array(
+			[
+				[r11, r12, r13, part_cur[0]],
+				[r21, r22, r23, part_cur[1]],
+				[r31, r32, r33, 0.93],
+				[0, 0, 0, 1]
+			])
+	
+	return t_w2b
+
 
 def convertFrame(part_cur, ori_robot, head_angles):
-	r, p, y = ori_robot[0], ori_robot[1], part_cur[2]
-	r11 = np.cos(y) * np.cos(p)
-	r12 = np.cos(y) * np.sin(p) * np.sin(r) - np.sin(y) * np.cos(r)
-	r13 = np.cos(y) * np.sin(p) * np.cos(r) + np.sin(y) * np.sin(r)
+	cosHA0 = np.cos(head_angles[0])
+	sinHA0 = np.sin(head_angles[0])
 
-	r21 = np.sin(y) * np.cos(p)
-	r22 = np.sin(y) * np.sin(p) * np.sin(r) + np.cos(y) * np.cos(r)
-	r23 = np.sin(y) * np.sin(p) * np.cos(r) - np.cos(y) * np.sin(r)
-
-	r31 = -np.sin(p)
-	r32 = np.cos(p) * np.sin(r)
-	r33 = np.cos(p) * np.cos(r)
+	cosHA1 = np.cos(head_angles[1])
+	sinHA1 = np.sin(head_angles[1])
 	
-	t_w2b = np.array([[r11, r12, r13, part_cur[0]],
-										[r21, r22, r23, part_cur[1]],
-										[r31, r32, r33, 0.93],
-										[0, 0, 0, 1]])
+	t_w2b = getW2B(part_cur, ori_robot)
 
-	t_b2h = np.array([[np.cos(head_angles[0]), -np.sin(head_angles[0]), 0, 0],
-										[np.sin(head_angles[0]), np.cos(head_angles[0]), 0, 0],
-										[0, 0, 1, 0.33],
-										[0, 0, 0, 1]])
+	t_b2h = np.array([[cosHA0, -sinHA0, 0, 0],
+						[sinHA0, cosHA0, 0, 0],
+						[0, 0, 1, 0.33],
+						[0, 0, 0, 1]])
 
-	t_h2l = np.array([[np.cos(head_angles[1]), 0, np.sin(head_angles[1]), 0],
-										[0, 1, 0, 0],
-										[-np.sin(head_angles[1]), 0, np.cos(head_angles[1]), 0.15],
-										[0, 0, 0, 1]])
-
-	T = np.einsum('ij,jk,kl->il', t_w2b, t_b2h, t_h2l)
-	return T
+	t_h2l = np.array([[cosHA1, 0, sinHA1, 0],
+						[0, 1, 0, 0],
+						[-sinHA1, 0, cosHA1, 0.15],
+						[0, 0, 0, 1]])
+	
+	return np.matmul(t_w2b, t_b2h, t_h2l)
 
 
 def mapConvert(scan, ori_robot, head_a, angles, particles, N, pos_phy, posX_map, posY_map, m):
@@ -58,15 +81,33 @@ def mapConvert(scan, ori_robot, head_a, angles, particles, N, pos_phy, posX_map,
 	scan_valid = scan[indValid]
 	angles_valid = angles[indValid]
 
-	xs0 = np.array([np.einsum('i,i->i', scan_valid, np.cos(angles_valid))])
-	ys0 = np.array([np.einsum('i,i->i', scan_valid, np.sin(angles_valid))])
+	xs0 = np.array([scan_valid * np.cos(angles_valid)])
+	ys0 = np.array([scan_valid * np.sin(angles_valid)])
 
 	Y = np.concatenate([np.concatenate([np.concatenate([xs0, ys0], axis=0), np.zeros(xs0.shape)], axis=0), np.ones(xs0.shape)], axis=0)
-	
-	for i in range(N):
-		trans_cur = convertFrame(particles[i, :], ori_robot, head_a)
 
-		res = np.einsum('ij,jk->ik', trans_cur, Y)
+	cosHA0 = np.cos(head_a[0])
+	sinHA0 = np.sin(head_a[0])
+
+	cosHA1 = np.cos(head_a[1])
+	sinHA1 = np.sin(head_a[1])
+
+	t_b2h = np.array([[cosHA0, -sinHA0, 0, 0],
+					[sinHA0, cosHA0, 0, 0],
+					[0, 0, 1, 0.33],
+					[0, 0, 0, 1]])
+
+	t_h2l = np.array([[cosHA1, 0, sinHA1, 0],
+						[0, 1, 0, 0],
+						[-sinHA1, 0, cosHA1, 0.15],
+						[0, 0, 0, 1]])
+
+	t_b2l = np.matmul(t_b2h, t_h2l)
+
+	for i in range(N):
+		trans_cur = np.matmul(getW2B(particles[i,:], ori_robot), t_b2l)
+
+		res = np.matmul(trans_cur, Y)
 		ind_notG = res[2, :] > 0.1
 
 		pos_phy[i] = res[0 : 2, ind_notG]
